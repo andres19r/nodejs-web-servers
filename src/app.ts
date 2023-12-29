37ecc4c2e75c330c11318e0d2f038eb9ea8b62b4
Andres Rivero <andres19rivero@gmail.com>
2023-12-29 18:23:39 -0400

    feat: create an http server to serve an html file

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hello World')
+import fs from "fs";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // res.write(`<h1>URL ${req.url}</h1>`);
+  // res.end();
+
+  // const data = { name: "John Doe", age: 30, city: "New York" };
+  // res.writeHead(200, { "Content-Type": "application/json" });
+  // res.end(JSON.stringify(data));
+
+  if (req.url === "/") {
+    const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlFile);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end();
+  }
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
