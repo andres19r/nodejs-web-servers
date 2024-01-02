cb63a6754f127f3493b8226491388b1498d59ed6
Andres Rivero <andres19rivero@gmail.com>
2024-01-02 13:59:03 -0400

    feat(todos): complete crud

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -7,6 +7,10 @@ export class TodoRoutes {
     const todoController = new TodosController();
 
     router.get("/", todoController.getTodos);
+    router.post("/", todoController.createTodo);
+    router.put("/:id", todoController.updateTodo);
+    router.get("/:id", todoController.getTodoById);
+    router.delete("/:id", todoController.deleteTodo);
     return router;
   }
 }
